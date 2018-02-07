e4a29f2619cf853d7959e96d0c226ba484ed4080
Ganeshbabu Thavasimuthu <gxt.gatsby@Ganeshbabus-MacBook-Pro.local>
2018-02-06 22:46:43 -0600

    basic ui

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Shareitpro' });
 });
 
+
+router.post('/login', function(req, res, next) {
+  console.log('b %o', req.body.action);
+  if (req.body.action=='view') {
+    res.render('view.pug', { name: req.body.username });
+  } else {
+    res.render('create.pug', { name: req.body.username });
+  }
+});
+
 module.exports = router;
