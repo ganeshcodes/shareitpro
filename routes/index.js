d0569ff017afe7c8d02acadc34da17c807c6931b
Ganeshbabu Thavasimuthu <gxt.gatsby@Ganeshbabus-MacBook-Pro.local>
2018-02-11 14:34:29 -0600

    view photots shared by others

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var azure = require('azure-storage');
 var Duplex = require('stream').Duplex;  
+var db = require('../lib/db');
+var Request = require('tedious').Request;
 
 var azureblob = azure.createBlobService(
   'shareitfiles',
@@ -27,16 +29,41 @@ router.get('/', function(req, res, next) {
 
 router.post('/login', function(req, res, next) {
   console.log('b %o', req.body.action);
+  var username = req.body.username;
   if (req.body.action=='view') {
-    res.render('view.pug', { name: req.body.username });
+    var results = [];
+    // Query all the photos shared by other users
+    var query = "select * from shareitmain where username!='"+username+"'";
+    var request = new Request(query, function(err,rowcount,rows){
+      if (err){
+        console.log('error %o',err);
+        var error = {
+          text: 'Something went wrong. Try again later!', 
+          style: 'alert alert-danger'
+        };
+        res.render('view.pug', { name: req.body.username, message: error });
+      }
+      console.log('rowcount = %o',rowcount);
+      console.log('rows = %o',results);
+      res.render('view.pug', { name: username, message:{text:''}, rows: results });
+    });
+    request.on('row', function(columns) {
+      var row = {};
+      columns.forEach(function(column) {
+          console.log("%s\t%s", column.metadata.colName, column.value);
+          row[column.metadata.colName] = column.value;
+      });
+      results.push(row);
+    });
+    db.execSql(request);
   } else {
-    res.render('create.pug', { name: req.body.username, message: {text: ''} });
+    res.render('create.pug', { name: username, message: {text: ''} });
   }
 });
 
 router.post('/create', multer.single('photo'), function (req, res, next) {
   console.log('path : %o',req.file);
-  // Upload photo to azure blob storage
+  // Upload photo to azure blob storage and the details to database
   azureblob.createBlockBlobFromStream('shareitcontainer', req.file.originalname, getStream(req.file.buffer), req.file.size, function(error, result, response){
     if(!error){
       // file uploaded
@@ -46,21 +73,41 @@ router.post('/create', multer.single('photo'), function (req, res, next) {
       var title = req.body.title;
       var filename = result.name;
       var modified = result.lastModified;
-      var ratings = 0; // No rating so far since this is the first time picture is shared
+      var ratings = 0, ratingscount = 0;
       var filelink = azurestorageURL + encodeURIComponent(filename);
+      // Create a query to insert row
+      var query = "INSERT INTO shareitmain(username, title, modified, ratings, filelink, ratingscount) VALUES('"+username+"','"+title+"','"+modified+"','"+ratings+"','"+filelink+"','"+ratingscount+"')";
+      var request = new Request(query, function(err,rowcount,rows){
+        if (err){
+          console.log('error %o',err);
+          var error = {
+            text: 'Something went wrong. Try again later!', 
+            style: 'alert alert-danger'
+          };
+          res.render('create.pug', { name: req.body.username, message: error });
+        }
+        console.log(rowcount + ' row(s) returned');
+        var success = {
+          text: 'Photo shared successfully. Try sharing another one!', 
+          style: 'alert alert-success'
+        };
+        res.render('create.pug', { name: req.body.username, message: success });
+      });
+      /*request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+         });
+      });*/
+      db.execSql(request);
       console.log(username+' '+title+' '+filename+' '+modified+' '+filelink);
-      var msg = {
-        text: 'Great! Share another picture!', 
-        style: 'alert alert-success'
-      };
     } else {
       console.error("error : %o", error);
       var msg = {
         text: 'Oops! Something went wrong!', 
         style: 'alert alert-danger'
       };
+      res.render('create.pug', { name: req.body.username, message: msg});
     }
-    res.render('create.pug', { name: req.body.username, message: msg});
   });
 });
 
